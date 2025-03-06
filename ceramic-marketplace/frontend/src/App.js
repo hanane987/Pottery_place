dad8318ad3d307c12c25599e77492e9c210de115
hanane987 <aminahanane987@gmail.com>
2025-03-06 23:12:44 +0100

    feat(POT-66): add routes for login and register page

---
+++ ceramic-marketplace/frontend/src/App.js
@@ -0,0 +1,22 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './user/Login';
+import Register from './user/Register';
+
+
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+            <Route path="/" element={<LandingPage />} />
+            
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+               
+            </Routes>
+        </Router>
+    );
+};
+
+export default App;
