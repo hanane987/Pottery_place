7f3180c50e3b925351b8adc3685379fb197a3ccd
hanane987 <aminahanane987@gmail.com>
2025-03-07 14:42:59 +0100

    Save untracked files before merge

---
+++ ceramic-marketplace/frontend/src/user/VendeurDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VendeurDashboard = () => {
+    return (
+        <div>
+            <h1>Vendeur Dashboard</h1>
+            <p>Welcome, Vendeur!</p>
+        </div>
+    );
+};
+
+export default VendeurDashboard;
