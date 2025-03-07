6f2b4b8f9b21dc29c079f85e1437b78dc69005a0
hanane987 <aminahanane987@gmail.com>
2025-03-07 13:26:23 +0100

    feat(POT-23): add model attribut for the contact

---
+++ ceramic-marketplace/backend/models/Contact.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        match: /.+\@.+\..+/ 
+    },
+    subject: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true }); 
+
+const ContactMessage = mongoose.model('ContactMessage', contactSchema);
+
+module.exports = ContactMessage;
