9ff0ac675bacad1f1d71f6c1bd2b40c8b74b50e2
hanane987 <aminahanane987@gmail.com>
2025-03-21 15:43:54 +0100

    Docs(POT-22): add the ban user function

--- ceramic-marketplace/backend/controllers/userController.js
+++ ceramic-marketplace/backend/controllers/userController.js
@@ -32,3 +32,13 @@ exports.getStatistics = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports.banUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.userId, { is_banned: true }, { new: true });
+        if (!user) return res.status(404).json({ message: 'User not found' });
+        res.json(user);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
