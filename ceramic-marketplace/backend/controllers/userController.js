04ae70c68c9202ad365d968bde41b00daa551f19
hanane987 <aminahanane987@gmail.com>
2025-03-21 15:46:34 +0100

    Docs(POT-22): add the function to get all the users

--- ceramic-marketplace/backend/controllers/userController.js
+++ ceramic-marketplace/backend/controllers/userController.js
@@ -52,3 +52,12 @@ exports.unbanUser = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find(); 
+        res.json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
