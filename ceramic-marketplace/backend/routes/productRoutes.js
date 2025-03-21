541f1aa74f0c22ab3016c5d77cdebceef56dc11c
hanane987 <aminahanane987@gmail.com>
2025-03-21 16:13:41 +0100

    feat(POT-22):add the get all product route

--- ceramic-marketplace/backend/routes/productRoutes.js
+++ ceramic-marketplace/backend/routes/productRoutes.js
@@ -1,23 +1,12 @@
 const express = require('express');
-const multer = require('multer');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/images');
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-});
-
-const upload = multer({ storage: storage });
-
-router.get('/', productController.getProducts);
-router.post('/', upload.array('images'), productController.createProduct);
-router.put('/:id', upload.array('images'), productController.updateProduct);
-router.delete('/:id', productController.deleteProduct);
+router.get('/all', productController.getAllProducts);
+router.get('/', productController.getProductsByVendor);
 router.get('/:id', productController.getProductById);
+router.post('/', productController.createProduct); 
+router.put('/:id', productController.updateProduct); 
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
